8cefd7af94d07608cd7b73fa822326412a3f269f
KoalaThee <koalathee@gmail.com>
2024-12-23 08:04:20 +0100

    Updated Firebase Storage Plugins and Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.database)
     implementation(libs.firebase.database.ktx)
+    implementation(libs.firebase.storage)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
