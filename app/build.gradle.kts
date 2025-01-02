e1e3d602009cb8a324b6be82acc6ed02b35cc665
Giov4 <brancaccio@protonmail.com>
2025-01-02 22:30:06 +0100

    Optimized building process and reduced minSdk to 34, to support more android phones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.fieldwise"
-        minSdk = 35
+        minSdk = 34
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
