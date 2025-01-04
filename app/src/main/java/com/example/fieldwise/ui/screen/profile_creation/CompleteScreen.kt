1fbe6f5e02029b68e13e4d4d5256f878615ed827
xbethpolar <bethsyiliannyme@gmail.com>
2025-01-04 23:29:44 +0100

    Add Expand Animation in CompleteScreen and ExerciseCompleteScreen

--- app/src/main/java/com/example/fieldwise/ui/screen/profile_creation/CompleteScreen.kt
+++ app/src/main/java/com/example/fieldwise/ui/screen/profile_creation/CompleteScreen.kt
@@ -1,5 +1,8 @@
 package com.example.fieldwise.ui.screen.profile_creation
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +17,10 @@ import androidx.compose.material.icons.rounded.CheckCircle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,9 +35,33 @@ import com.example.fieldwise.ui.theme.FieldWiseTheme
 import com.example.fieldwise.ui.theme.InterFontFamily
 import com.example.fieldwise.ui.widget.MainButton
 import com.example.fieldwise.ui.widget.MainButtonType
+import kotlinx.coroutines.launch
 
 @Composable
 fun CompleteScreen(modifier: Modifier = Modifier, NavigateToHome: () -> Unit) {
+
+    //animation for background image
+    val backgroundAn = remember { Animatable(100f) }
+    val checkAn = remember { Animatable(0f)}
+
+    LaunchedEffect(Unit) {
+            launch {  backgroundAn.animateTo(
+                targetValue = 6000f,
+                animationSpec = tween(
+                    durationMillis = 1500
+                )
+            ) }
+
+            launch {  checkAn.animateTo(
+                targetValue = 200f,
+                animationSpec = tween(
+                    durationMillis = 1000
+                )
+            )}
+    }
+
+
+    //content
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -46,6 +77,7 @@ fun CompleteScreen(modifier: Modifier = Modifier, NavigateToHome: () -> Unit) {
                 painter = painterResource(id = R.drawable.bg1),
                 contentDescription = "Complete background",
                 modifier = Modifier
+                    .size(backgroundAn.value.dp) //animated size
                     .fillMaxSize() // Make sure the image takes the full size of the Box
                     .align(Alignment.Center) // Center the image
             )
@@ -59,7 +91,7 @@ fun CompleteScreen(modifier: Modifier = Modifier, NavigateToHome: () -> Unit) {
                     painter = painterResource(id=R.drawable.checkicon_green),
                     contentDescription = "Check Icon Blue",
                     modifier = Modifier
-                        .size(150.dp)
+                        .size(checkAn.value.dp)
                 )
                 Spacer(modifier = Modifier.height(80.dp))
                 Text(
