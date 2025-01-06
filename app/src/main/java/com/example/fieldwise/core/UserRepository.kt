9b80e484fde983d4cf566bb7fd18542b281b6426
Aleksandra <106492098+olala10@users.noreply.github.com>
2025-01-06 18:04:04 +0100

    Local database update

---
+++ app/src/main/java/com/example/fieldwise/core/UserRepository.kt
@@ -0,0 +1,18 @@
+package com.example.fieldwise.data
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class UserRepository(private val userProfileDao: UserProfileDao) {
+
+    //checking if the user already exists
+    suspend fun isFirstTimeUser(username: String): Boolean = withContext(Dispatchers.IO) {
+        val userProfile = userProfileDao.getUserProfile(username)
+        return@withContext userProfile == null
+    }
+
+    //saving new user method
+    suspend fun saveUserProfile(userProfile: UserProfile) = withContext(Dispatchers.IO) {
+        userProfileDao.insertUserProfile(userProfile)
+    }
+}
